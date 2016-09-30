7cbd1f34abb4dc53b641def1e24a043f45ad5afc
Apoorv Mehta <mehta.apoorv@hotmail.com>
2016-09-30 01:56:25 -0700

    State Pattern Draft

---
+++ Greenfoot/ReadyToPlayState.java
@@ -0,0 +1,36 @@
+/**
+ * Write a description of class ReadyToPlayState here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ReadyToPlayState extends GameState 
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class ReadyToPlayState
+     */
+    public ReadyToPlayState()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    public String toString()
+    {
+        return "Ready to Play!";
+    }
+    
+}
