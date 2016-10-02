4b195b5b5b6f1f1cad92425ee2e3e190a8a3a249
Parvez <parvez.patel@sjsu.edu>
2016-10-01 22:16:31 -0700

    issue #11 done

---
+++ Greenfoot/Circuit.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Circuit here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Circuit extends Actor
+{
+    /**
+     * Act - do whatever the Circuit wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }
+    
+    public String endcodeBits(String inputString){
+        //logic to encode the String
+        return "sampleString";
+    }
+}
