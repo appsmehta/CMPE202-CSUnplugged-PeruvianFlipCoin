6f06ecf9ed4024307ba2ce48b688ea3eff7a9b53
Apoorv Mehta <mehta.apoorv@hotmail.com>
2016-10-07 14:40:56 -0700

    Implemented Prepare method for the world

--- Greenfoot/CaptainB.java
+++ Greenfoot/CaptainB.java
@@ -1,15 +1,21 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Captain here.
+ * Write a description of class CaptainB here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Captain extends Player
+public class CaptainB extends Player
 {
+    
+    public CaptainB(int pNum)
+        {
+            super(pNum);
+           // setImage(images[pNum]);
+        }  
     /**
-     * Act - do whatever the Captain wants to do. This method is called whenever
+     * Act - do whatever the CaptainB wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
--- Greenfoot/MyWorld.java
+++ Greenfoot/MyWorld.java
@@ -17,6 +17,28 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-      
+
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        
+        Player player1 = new Player(0);
+        addObject(player1, 372, 331);
+        Player player2 = new Player(1);
+        addObject(player2, 561, 331);
+        Player player3 = new Player(2);
+         addObject(player3, 237, 64);
+        Player player4 = new Player(3);
+        addObject(player4, 44, 64);
+        CaptainA capA = new CaptainA(5);
+        addObject(capA, 137, 205);
+        CaptainB capB = new CaptainB(4);
+        addObject(capB, 461, 205);
     }
 }
