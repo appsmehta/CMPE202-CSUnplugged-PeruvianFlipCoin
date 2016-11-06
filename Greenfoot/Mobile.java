daeb13bcea78b7ff5a629d99a49717503b1ea7ed
yugalk14 <Yugal@Yugals-MacBook-Pro.local>
2016-11-05 23:52:00 -0700

    new mobile image added

--- Greenfoot/Mobile.java
+++ Greenfoot/Mobile.java
@@ -8,6 +8,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Mobile extends Actor
 {
+    static GreenfootImage[] images={ new GreenfootImage("mob1.png"),
+                                     new GreenfootImage("mob2.png")};
+    int mobile;
+    public Mobile(int mobNum)
+    {
+        mobile= mobNum;
+        setImage(images[mobNum]);
+    }
     /**
      * Act - do whatever the Mobile wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
